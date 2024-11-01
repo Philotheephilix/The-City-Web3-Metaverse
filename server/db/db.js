7f64af0e67feba4b7b78d3ced1f11448c0fadae7
PhiloTheePhilix <110274378+Philotheephilix@users.noreply.github.com>
2024-11-02 00:41:20 +0530

    added vercel support for backend

--- server/db/db.js
+++ server/db/db.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
 const { MongoClient } = require('mongodb');
 
-const uri = 'mongodb://localhost:27017';
+const uri = process.env.MONGODB_URI;
 const client = new MongoClient(uri);
 let db;
 
